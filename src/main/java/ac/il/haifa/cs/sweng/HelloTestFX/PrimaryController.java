960b619feaad38c2f615db53617f9926eeff9b63
Liran <liranbd1@gmail.com>
2021-12-31 19:48:37 +0200

    Junit

---
+++ src/main/java/ac/il/haifa/cs/sweng/HelloTestFX/PrimaryController.java
@@ -0,0 +1,17 @@
+package ac.il.haifa.cs.sweng.HelloTestFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    void sayHello(ActionEvent event) {
+
+    }
+
+}
---
+++ src/test/java/ac/il/haifa/cs/sweng/HelloTestFX/AppTest.java
@@ -0,0 +1,48 @@
+package ac.il.haifa.cs.sweng.HelloTestFX;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.matcher.control.TextInputControlMatchers;
+
+@ExtendWith(ApplicationExtension.class)
+public class AppTest {
+
+  private static Parent loadFXML(String fxml) throws IOException {
+    var loader = new FXMLLoader(App.class.getResource("primary.fxml"));
+    return loader.load();
+  }
+
+  @Start
+  public void start(Stage stage) throws IOException{
+    var scene = new Scene(loadFXML("primary"), 640,480);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @Test
+  void test_label_is_empty(FxRobot robot){
+    FxAssert.verifyThat("#textField", TextInputControlMatchers.hasText(""));
+  }
+
+  @Test
+  void test_button_test(FxRobot robot){
+    FxAssert.verifyThat(".button", LabeledMatchers.hasText("Say Hello"));
+  }
+
+  @Test
+  void test_button_click(FxRobot robot){
+    robot.clickOn(".button");
+
+    FxAssert.verifyThat("#textField", TextInputControlMatchers.hasText("Hello World"));
+  }
+}
