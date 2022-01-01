6632a795b6d9f74f335d1fa6183598fff905ba08
Liran <liranbd1@gmail.com>
2022-01-01 12:20:22 +0200

    Finished

--- src/main/java/ac/il/haifa/cs/sweng/HelloTestFX/PrimaryController.java
+++ src/main/java/ac/il/haifa/cs/sweng/HelloTestFX/PrimaryController.java
@@ -2,6 +2,7 @@ package ac.il.haifa.cs.sweng.HelloTestFX;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class PrimaryController {
--- src/test/java/ac/il/haifa/cs/sweng/HelloTestFX/AppTest.java
+++ src/test/java/ac/il/haifa/cs/sweng/HelloTestFX/AppTest.java
@@ -1,6 +1,7 @@
 package ac.il.haifa.cs.sweng.HelloTestFX;
 
 import java.io.IOException;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
